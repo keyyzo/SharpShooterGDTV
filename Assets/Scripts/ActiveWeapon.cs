df696950bbbf92492cc3d7c1cae082e8d17f34f8
keyyzo <58720632+keyyzo@users.noreply.github.com>
2025-03-31 02:25:12 +0100

    added fire rate functionality to the active weapon

---
+++ Assets/Scripts/ActiveWeapon.cs
@@ -0,0 +1,72 @@
+using StarterAssets;
+using UnityEngine;
+
+public class ActiveWeapon : MonoBehaviour
+{
+
+    [SerializeField] WeaponSO weaponSO;
+    
+
+    const string SHOOT_STRING = "Shoot";
+
+    StarterAssetsInputs starterAssetsInputs;
+    Weapon currentWeapon;
+    Animator animator;
+
+    bool canShoot = true;
+    float shootTimer = 0.0f;
+
+    private void Awake()
+    {
+        starterAssetsInputs = GetComponentInParent<StarterAssetsInputs>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        currentWeapon = GetComponentInChildren<Weapon>();
+    }
+
+    private void Update()
+    {
+        HandleShoot();
+        HandleFireRate();
+
+    }
+
+    private void HandleShoot()
+    {
+        if (!starterAssetsInputs.shoot)
+            return;
+
+        if (canShoot)
+        {
+            currentWeapon.Shoot(weaponSO);
+            animator.Play(SHOOT_STRING, 0, 0f);
+            canShoot = false;
+
+            
+        }
+
+
+        starterAssetsInputs.ShootInput(false);
+
+    }
+
+    void HandleFireRate()
+    {
+        if (!canShoot)
+        {
+            shootTimer += Time.deltaTime;
+            Debug.Log("Chambering...");
+        }
+        
+
+        if (shootTimer >= weaponSO.FireRate)
+        {
+            canShoot = true;
+            shootTimer = 0.0f;
+            Debug.Log("Can Shoot!");
+        }
+    }
+}
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,46 +1,24 @@
-using StarterAssets;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    [SerializeField] WeaponSO weaponSO;
-    [SerializeField] int damagePerBullet = 1;
+    
     [SerializeField] ParticleSystem muzzleFlash;
-    [SerializeField] GameObject hitVFXPrefab;
-    [SerializeField] Animator animator;
 
-    const string SHOOT_STRING = "Shoot";
-
-    StarterAssetsInputs starterAssetsInputs;
-
-    private void Awake()
+    public void Shoot(WeaponSO weaponSO)
     {
-        starterAssetsInputs = GetComponentInParent<StarterAssetsInputs>();
-    }
-
-    private void Update()
-    {
-        HandleShoot();
-
-    }
-
-    private void HandleShoot()
-    {
-        if (!starterAssetsInputs.shoot)
-            return;
-
+        
         muzzleFlash.Play();
-        animator.Play(SHOOT_STRING, 0, 0f);
-        starterAssetsInputs.ShootInput(false);
-
+       
         RaycastHit hit;
 
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
         {
 
+
             Vector3 hitSpawnPos = hit.point;
 
-            GameObject hitVFXGO = Instantiate(hitVFXPrefab, hitSpawnPos, Quaternion.identity);
+            GameObject hitVFXGO = Instantiate(weaponSO.HitVFXPrefab, hitSpawnPos, Quaternion.identity);
 
             EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
             enemyHealth?.TakeDamage(weaponSO.Damage);
