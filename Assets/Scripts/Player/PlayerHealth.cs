065ec9f1031cd187ddcc15693a86d2b051658eb1
keyyzo <58720632+keyyzo@users.noreply.github.com>
2025-04-09 00:51:35 +0100

    added a death camera for a death camera transition, and some UI to represent player health

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,24 +1,53 @@
+using Cinemachine;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
+    [Range(1,10)]
     [SerializeField] int startingHealth = 5;
+    [SerializeField] CinemachineVirtualCamera deathVirtualCamera;
+    [SerializeField] Transform weaponCameraTransform;
+    [SerializeField] Image[] shieldBars;
 
     int currentHealth;
+    int gameOverVirtualCameraPriority = 20;
 
     private void Awake()
     {
         currentHealth = startingHealth;
+
+        AdjustHealthUI();
     }
 
     public void TakeDamage(int damageToTake)
     {
         currentHealth -= damageToTake;
+        AdjustHealthUI();
+
 
         if (currentHealth <= 0)
         {
-            //Destroy(this.gameObject);
+            weaponCameraTransform.parent = null;
+            deathVirtualCamera.Priority = gameOverVirtualCameraPriority;
+            Destroy(this.gameObject);
             Debug.Log("Player has died!");
+        }
+    }
+
+    void AdjustHealthUI()
+    {
+        for (int i = 0; i < shieldBars.Length; i++)
+        {
+            if (i < currentHealth)
+            {
+                shieldBars[i].gameObject.SetActive(true);
+            }
+
+            else
+            {
+                shieldBars[i].gameObject.SetActive(false);
+            }
         }
     }
 }
